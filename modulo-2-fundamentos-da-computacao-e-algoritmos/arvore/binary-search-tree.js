433b5814f1e120defffb941f0658f0805e106f2e
Laurindo Calixto <laurindocalixto@gmail.com>
2025-04-06 12:48:56 -0300

    update arvore de pesquisa binaria

--- modulo-2-fundamentos-da-computacao-e-algoritmos/arvore/binary-search-tree.js
+++ modulo-2-fundamentos-da-computacao-e-algoritmos/arvore/binary-search-tree.js
@@ -11,58 +11,134 @@ export default class BinarySearchTree {
         if (this.root == null) {
             this.root = new Node(key);
         } else {
-            this.insertNode(this.root, key);
+            this.#insertNode(this.root, key);
         }
     }
 
-    insertNode(node, key) {
+    #insertNode(node, key) {
         if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
             if (node.left == null) {
                 node.left = new Node(key);
             } else {
-                this.insertNode(node.left, key);
+                this.#insertNode(node.left, key);
             }
         } else {
             if (node.right == null) {
                 node.right = new Node(key);
             } else {
-                this.insertNode(node.right, key);
+                this.#insertNode(node.right, key);
             }
         }
     }
 
     inOrderTraverse(callback) {
-        this.inOrderTraverseNode(this.root, callback);
+        this.#inOrderTraverseNode(this.root, callback);
     }
 
-    inOrderTraverseNode(node, callback) {
+    #inOrderTraverseNode(node, callback) {
         if (node != null) {
-            this.inOrderTraverseNode(node.left, callback);
+            this.#inOrderTraverseNode(node.left, callback);
             callback(node.key);
-            this.inOrderTraverseNode(node.right, callback);
+            this.#inOrderTraverseNode(node.right, callback);
         }
     }
 
     preOrderTraverse(callback) {
-        this.preOrderTraverseNode(this.root, callback);
+        this.#preOrderTraverseNode(this.root, callback);
     }
 
-    preOrderTraverseNode(node, callback) {
+    #preOrderTraverseNode(node, callback) {
         if (node != null) {
             callback(node.key);
-            this.preOrderTraverseNode(node.left, callback);
-            this.preOrderTraverseNode(node.right, callback);
+            this.#preOrderTraverseNode(node.left, callback);
+            this.#preOrderTraverseNode(node.right, callback);
         }
     }
 
     postOrderTraverse(callback) {
-        this.postOrderTraverseNode(this.root, callback);
+        this.#postOrderTraverseNode(this.root, callback);
     }
-    postOrderTraverseNode(node, callback) {
+    #postOrderTraverseNode(node, callback) {
         if (node != null) {
-            this.postOrderTraverseNode(node.left, callback);
-            this.postOrderTraverseNode(node.right, callback);
+            this.#postOrderTraverseNode(node.left, callback);
+            this.#postOrderTraverseNode(node.right, callback);
             callback(node.key);
+        }
+    }
+    min() {
+        return this.#minNode(this.root);
+    }
+
+    #minNode(node) {
+        let current = node;
+        while (current != null && current.left != null) {
+            current = current.left;
+        }
+        return current;
+    }
+
+    max() {
+        return this.#maxNode(this.root);
+    }
+    #maxNode(node) {
+        let current = node;
+        while (current != null && current.right != null) {
+            current = current.right;
+        }
+        return current;
+    }
+
+    search(key) {
+        return this.#searchNode(this.root, key);
+    }
+
+    #searchNode(node, key) {
+        if (node == null) {
+            return false;
+        }
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            return this.#searchNode(node.left, key);
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            return this.#searchNode(node.right, key);
+        } else {
+            return true;
+        }
+    }
+
+    remove(key) {
+        this.root = this.#removeNode(this.root, key);
+    }
+
+    #removeNode(node, key) {
+        if (node == null) {
+            return null;
+        }
+        if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            node.left = this.#removeNode(node.left, key);
+            return node;
+        } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            node.right = this.#removeNode(node.right, key);
+            return node;
+        } else {
+            //key é igual a node.item
+            //caso 1
+            if (node.left == null && node.right == null) {
+                node = null;
+                return node;
+            }
+            //caso 2
+            if (node.left == null) {
+                node = node.right;
+                return node;
+            } else if (node.right == null) {
+                node = node.left;
+                return node;
+            }
+            //caso 3
+            const aux = this.#minNode(node.right);
+            node.key = aux.key;
+            node.right = this.#removeNode(node.right, aux.key);
+            return node;
         }
     }
 }
@@ -84,5 +160,14 @@ tree.insert(20);
 tree.insert(18);
 tree.insert(25);
 tree.insert(6);
-
 tree.postOrderTraverse(printNode);
+console.log('*********');
+
+tree.remove(11);
+tree.postOrderTraverse(printNode);
+console.log(tree.min().key);
+console.log(tree.max().key);
+const elements = [6, 8];
+console.log(tree.search(elements[0]) ? `Key ${elements[0]} found.` : `Key ${elements[0]} not found.`);
+console.log(tree.search(elements[1]) ? `Key ${elements[1]} found.` : `Key ${elements[1]} not found.`);
+console.log(tree.root.key);
