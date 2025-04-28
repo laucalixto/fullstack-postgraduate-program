341edd413a5cdd18faade6488ee8663a3b00ee21
Laurindo Calixto <laurindocalixto@gmail.com>
2025-04-28 10:50:25 -0300

    algoritmo de ordenação merge sort

---
+++ modulo-2-fundamentos-da-computacao-e-algoritmos/algoritmosOrdenacao/merge-sort.js
@@ -0,0 +1,26 @@
+import { Compare, defaultCompare } from './utilities/util.js';
+
+function mergeSort(array, compareFn = defaultCompare) {
+    if (array.length > 1) {
+        const { length } = array;
+        const middle = Math.floor(length / 2);
+        const left = mergeSort(array.slice(0, middle), compareFn);
+        const right = mergeSort(array.slice(middle, length), compareFn);
+        array = merge(left, right, compareFn);
+    }
+    return array;
+}
+
+function merge(left, right, compareFn) {
+    let i = 0;
+    let j = 0;
+    const result = [];
+    while (i < left.length && j < right.length) {
+        result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++]);
+    }
+    return result.concat(i < left.length ? left.slice(i) : right.slice(j));
+}
+
+// Exemplo de uso:
+const arrayMerge = [5, 2, 4, 1, 8];
+console.log('Merge Sort:', mergeSort(arrayMerge)); // Saída: [1, 2, 4, 5, 8]
